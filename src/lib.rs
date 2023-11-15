296ab1de5aa65e5865470f6fdf28061b94504e9f
Bazyl <bhorsey16@gmail.com>
2023-11-15 13:45:11 -0600

    v0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,64 @@ use std::str::FromStr;
 use std::fmt::{self, Debug, Display, Formatter};
 use std::error::Error;
 
-/// Custom error type for input function.
+/// Custom error type for prompt function.
 #[derive(Debug)]
-pub enum InputError {
-    IoError(io::Error),
-    ParseError(String),
+pub enum PromptError<T: FromStr> 
+where
+    T::Err: Debug + Display,
+{
+    Io(io::Error),
+    ParseError(<T as FromStr>::Err),
 }
 
-impl Display for InputError {
+impl<T: FromStr> Display for PromptError<T>
+where
+    T::Err: Debug + Display,
+{
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            InputError::IoError(e) => write!(f, "IO error: {}", e),
-            InputError::ParseError(e) => write!(f, "Parse error: {}", e),
+            PromptError::Io(e) => write!(f, "IO error: {}", e),
+            PromptError::ParseError(e) => write!(f, "Parse error: {}", e),
         }
     }
 }
 
-impl Error for InputError {}
+impl<T: FromStr + Debug> Error for PromptError<T> 
+where
+    T::Err: Debug + Display,
+{
+}
 
-impl From<io::Error> for InputError {
+impl<T: FromStr> From<io::Error> for PromptError<T>
+where
+    T::Err: Debug + Display,
+{
     fn from(e: io::Error) -> Self {
-        InputError::IoError(e)
+        PromptError::Io(e)
     }
 }
 
-/// A function similar to Python's `input`, allowing for a prompt and reading a line from stdin.
-/// 
+/// A function to prompt for prompt and read a line from stdin, parsing it into a specified type.
+///
 /// # Arguments
 ///
-/// * `prompt` - An optional prompt to display before reading input.
-/// * `flush` - Whether to flush stdout after displaying the prompt.
-/// 
+/// * `prompt` - A prompt to display before reading prompt.
+///
 /// # Errors
 ///
 /// This function will return an error if there's an issue with reading from stdin
-/// or parsing the input into the desired type.
-pub fn input<T: FromStr>(prompt: Option<&str>, flush: bool) -> Result<T, InputError>
+/// or parsing the prompt into the desired type.
+pub fn prompt<T: FromStr>(prompt: &str) -> Result<T, PromptError<T>>
 where
-    T::Err: Display {
-    if let Some(p) = prompt {
-        print!("{}", p);
-        if flush {
-            io::stdout().flush()?;
-        }
-    }
+    T::Err: Debug + Display,
+{
+    io::stdout().write_all(prompt.as_bytes())?; // `print!` may `panic!`
+    io::stdout().flush()?; // Always flush after the prompt
 
-    let mut input = String::new();
-    match io::stdin().read_line(&mut input) {
-        Ok(0) => Err(InputError::IoError(io::Error::new(io::ErrorKind::UnexpectedEof, "EOF reached"))),
-        Ok(_) => input.trim().parse::<T>().map_err(|e| InputError::ParseError(e.to_string())),
+    let mut prompt = String::new();
+    match io::stdin().read_line(&mut prompt) {
+        Ok(0) => Err(PromptError::Io(io::Error::new(io::ErrorKind::UnexpectedEof, "EOF reached"))),
+        Ok(_) => prompt.trim().parse::<T>().map_err(PromptError::ParseError),
         Err(e) => Err(e.into()),
     }
 }
@@ -62,22 +71,22 @@ mod tests {
 
     #[test]
     fn get_int() {
-        let i: i32 = input(None, false).unwrap();
+        let i: i32 = prompt("").unwrap();
         println!("{}", i);
-        let j: i32 = input(None, true).unwrap();
+        let j: f32 = prompt("").unwrap();
         println!("{}", j);
 
     }
 
     #[test]
     fn get_int_2() {
-        let i: i32 = input(Some("Input: "), false).unwrap();
+        let i: i32 = prompt("Input: ").unwrap();
         print!("Output: {}", i);
     }
 
     #[test]
     fn hacker_rank() {
-        let s: String = input(None, true).unwrap();
+        let s: String = prompt("").unwrap();
         println!("hello, {}", s);
     }
 
