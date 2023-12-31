7d77246ec79226031c715007ad844070f67339e8
Bazyl <bhorsey16@gmail.com>
2023-12-31 02:56:46 -0600

    simplify and create macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,48 @@
 use std::io::{self, Write};
 use std::str::FromStr;
-use std::fmt::{self, Debug, Display, Formatter};
-use std::error::Error;
 
-/// Custom error type for prompt function.
-#[derive(Debug)]
-pub enum PromptError<T: FromStr> 
-where
-    T::Err: Debug + Display,
-{
-    Io(io::Error),
-    ParseError(<T as FromStr>::Err),
+/// Custom error type for input parsing.
+pub enum InputError {
+    IoError(io::Error),
+    ParseError(String),
 }
 
-impl<T: FromStr> Display for PromptError<T>
+/// Function to get input and parse it into the desired type.
+pub fn parse_input<T: FromStr>(prompt: &str) -> Result<T, InputError>
 where
-    T::Err: Debug + Display,
+    <T as FromStr>::Err: ToString,
 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            PromptError::Io(e) => write!(f, "IO error: {}", e),
-            PromptError::ParseError(e) => write!(f, "Parse error: {}", e),
-        }
-    }
+    print!("{}", prompt);
+    io::stdout().flush().map_err(InputError::IoError)?;
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).map_err(InputError::IoError)?;
+
+    input.trim().parse::<T>().map_err(|e| InputError::ParseError(e.to_string()))
 }
 
-impl<T: FromStr + Debug> Error for PromptError<T> 
-where
-    T::Err: Debug + Display,
-{
-}
-
-impl<T: FromStr> From<io::Error> for PromptError<T>
-where
-    T::Err: Debug + Display,
-{
-    fn from(e: io::Error) -> Self {
-        PromptError::Io(e)
-    }
-}
-
-/// A function to prompt for prompt and read a line from stdin, parsing it into a specified type.
-///
-/// # Arguments
-///
-/// * `prompt` - A prompt to display before reading prompt.
-///
-/// # Errors
-///
-/// This function will return an error if there's an issue with reading from stdin
-/// or parsing the prompt into the desired type.
-pub fn prompt<T: FromStr>(prompt: &str) -> Result<T, PromptError<T>>
-where
-    T::Err: Debug + Display,
-{
-    io::stdout().write_all(prompt.as_bytes())?; // `print!` may `panic!`
-    io::stdout().flush()?; // Always flush after the prompt
-
-    let mut prompt = String::new();
-    match io::stdin().read_line(&mut prompt) {
-        Ok(0) => Err(PromptError::Io(io::Error::new(io::ErrorKind::UnexpectedEof, "EOF reached"))),
-        Ok(_) => prompt.trim().parse::<T>().map_err(PromptError::ParseError),
-        Err(e) => Err(e.into()),
-    }
+/// Macro to simplify calling the parse_input function.
+#[macro_export]
+macro_rules! parse_input {
+    ($prompt:expr) => {
+        $crate::parse_input::<_>($prompt)
+    };
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
+    // Example test for successful parsing
     #[test]
-    fn get_int() {
-        let i: i32 = prompt("").unwrap();
-        println!("{}", i);
-        let j: f32 = prompt("").unwrap();
-        println!("{}", j);
-
+    fn test_parse_input_success() {
+        let result = "42".parse::<f64>();
+        assert_eq!(result, Ok(42.));
     }
 
+    // Example test for failed parsing
     #[test]
-    fn get_int_2() {
-        let i: i32 = prompt("Input: ").unwrap();
-        print!("Output: {}", i);
-    }
-
-    #[test]
-    fn hacker_rank() {
-        let s: String = prompt("").unwrap();
-        println!("hello, {}", s);
+    fn test_parse_input_failure() {
+        let result = "abc".parse::<i32>();
+        assert!(result.is_err());
     }
 
 }
